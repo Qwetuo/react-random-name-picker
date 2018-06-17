24cf466d75e729b22915492c2eb446a44e3db6e8
qwetuo <chewminzhuang@hotmail.com>
2018-06-17 17:01:21 +0800

    completed bonus

--- src/App.js
+++ src/App.js
@@ -1,40 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import ButtonOne from "./ButtonOne";
 import math from "mathjs";
-import ShowNames from "./ShowNames"
+import ShowNames from "./ShowNames";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      names: ["gordon","sahil","david","sally","jane","alice"],
-      luckyWinnerIndex: null
+      names: ["gordon", "sahil", "david", "sally", "jane", "alice"],
+      luckyWinnerIndex: null,
+      value: ""
     };
   }
   render() {
     return (
       <div>
         <h1 id="app-title">hello random name picker</h1>
-        {/* <ul>
-          {this.state.names.map((name,i) => {
-            if (this.state.luckyWinnerIndex === i) {
-              return <li id="winner">{name}</li>
-            } else {
-              return <li>{name}</li>
-            }
-          })}
-        </ul> */}
         <ShowNames input={this.state} />
+        <input
+          value={this.state.value}
+          onChange={event => {
+            this.handleChange(event);
+          }}
+        />
+        <button onClick={() => this.handleAddNameBtn()}>Add Names</button>
+        <br />
+        <br />
         <ButtonOne handleClick={() => this.handleClick()} />
         <br />
-        {/* <button onClick={()=> this.startNewList()}>Start you own list</button> */}
       </div>
     );
   }
   handleClick() {
-    const random = math.randomInt(this.state.names.length)
+    const random = math.randomInt(this.state.names.length);
     this.setState({
       luckyWinnerIndex: random
+    });
+  }
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    })
+  }
+  handleAddNameBtn() {
+    this.setState({
+      names: [...this.state.names, this.state.value],
+      value: ""
     })
   }
 }
